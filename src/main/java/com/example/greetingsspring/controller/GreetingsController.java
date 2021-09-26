7a2f11b187d985eebe85fff275b20a6a9258403d
rajshetye11 <rajshetye11@gmail.com>
2021-09-27 01:02:40 +0530

    [raj] : added ability to display hello + name

--- src/main/java/com/example/greetingsspring/controller/GreetingsController.java
+++ src/main/java/com/example/greetingsspring/controller/GreetingsController.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.greetingsspring.model.GreetingsModel;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingsController {
 	
 	@RequestMapping("/json")
@@ -18,4 +20,9 @@ public class GreetingsController {
 				new GreetingsModel("Abc","Xyz")
 						);
 	}
+	
+	 @RequestMapping("/hello")
+		public String home(@RequestParam String name) {
+			return "Hello "+name;
+		}
 }
