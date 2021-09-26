1da72abc7763aa4a1fa6b85b08fa990b140eb100
rajshetye11 <rajshetye11@gmail.com>
2021-09-27 00:49:49 +0530

    [raj] : added initial setup

---
+++ src/main/java/com/example/greetingsspring/GreetingsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsSpringApplication.class, args);
+	}
+
+}
